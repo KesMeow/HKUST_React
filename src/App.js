921ed77c3813e7a1f5b01651cb5aa4f111acb876
Kester Zhou <kesterzhou@gmail.com>
2020-08-26 13:28:56 -0400

    Uncontrolled Form

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main/>
       </div>
+      </BrowserRouter>
+      
     );
   }
 }
